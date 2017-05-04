fceba759ef416619be4afb68fc80472aacaa870e
collo <qollomcbizzy64@gmail.com>
2017-05-04 15:51:11 +0300

    added the backened and asigned value to the numericals

--- js/script.js
+++ js/script.js
@@ -1,20 +1,26 @@
 
-var isDigit = function(x){
   //checks if the number is a not a digit
-  if (x.match(/[0-9]/)){
-    return true;
+  var isDigit=function romanize(num) {
+    var lookup = {M:1000,CM:900,D:500,CD:400,C:100,XC:90,L:50,XL:40,X:10,IX:9,V:5,IV:4,I:1},roman = '',i;
+    for ( i in lookup ) {
+      while ( num >= lookup[i] ) {
+        roman += i;
+        num -= lookup[i];
+      }
+    }
+    return roman;
   }
-  else{
-    return false;
-  }
-};
-
 //jquery user interface
 $(document).ready(function(){
   $("form#numerical").submit(function(event){
     event.preventDefault();
-  var number=$("input#number").val();
-  var result=isDigit(number);
-  $("#result").text(result);
+  var number=parseInt($("input#number").val());
+  if(String(number).match(/[0-9]/)){
+    var result=isDigit(number);
+    $("#result").text(result);
+  }
+  else{
+    return "its not a number";
+  }
 });
 });
